66f1fa797e07ad63f164b3f364bac9fa16e689c5
Michał Dąbrowski <michal.dabrowski@daftcode.pl>
2019-09-03 15:35:37 +0200

    Update key parser for lint

--- Straal/Classes/Cryptor/KeyParser.swift
+++ Straal/Classes/Cryptor/KeyParser.swift
@@ -87,10 +87,10 @@ extension Data {
 		var bytes: [UInt8] = []
 		let length = hexString.distance(from: hexString.startIndex, to: hexString.endIndex)
 
-		for i in stride(from: 0, to: length, by: 2) {
-			let from = hexString.index(hexString.startIndex, offsetBy: i)
+		for index in stride(from: 0, to: length, by: 2) {
+			let from = hexString.index(hexString.startIndex, offsetBy: index)
 			if hexString.distance(from: from, to: hexString.endIndex) < 2 { return nil }
-			let to = hexString.index(hexString.startIndex, offsetBy: i+2)
+			let to = hexString.index(hexString.startIndex, offsetBy: index+2)
 			let num: UInt8? = UInt8(hexString[from..<to], radix: 16)
 			guard let unwrappedNum = num else { return nil }
 			bytes.append(unwrappedNum)

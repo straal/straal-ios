2d437ab73066b1faadbbd61d3f2bda12410f9d3c
Michał Dąbrowski <michal.dabrowski@daftcode.pl>
2020-12-31 14:03:05 +0100

    Add open url context

---
+++ Sources/Straal/URL/OpenURLContext.swift
@@ -0,0 +1,43 @@
+/*
+ * OpenURLContext.swift
+ * Created by Michał Dąbrowski on 31/12/2020.
+ *
+ * Straal SDK for iOS
+ * Copyright 2020 Straal Sp. z o. o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+import UIKit
+
+internal struct OpenURLContext: Equatable {
+	var url: URL
+	var sourceApplication: String?
+
+	init(uiOpenURLContext: UIOpenURLContext) {
+		url = uiOpenURLContext.url
+		sourceApplication = uiOpenURLContext.options.sourceApplication
+	}
+
+	init(url: URL, sourceApplication: String?) {
+		self.url = url
+		self.sourceApplication = sourceApplication
+	}
+}
+
+internal extension UIOpenURLContext {
+	var openURLContext: OpenURLContext {
+		.init(uiOpenURLContext: self)
+	}
+}

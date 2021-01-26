f43c74155ea2f7c93a05c3c68d004b1cc7ecac0d
Michał Dąbrowski <michal.dabrowski@daftcode.pl>
2021-01-26 19:32:36 +0100

    Add info dictionary fake

---
+++ Tests/StraalTests/Fakes/InfoDictionaryFake.swift
@@ -0,0 +1,42 @@
+/*
+ * InfoDictionaryFake.swift
+ * Created by Michał Dąbrowski on 26/01/2021.
+ *
+ * Straal SDK for iOS
+ * Copyright 2021 Straal Sp. z o. o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or  * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+@testable import Straal
+
+final class InfoDictionaryFake: InfoDictionaryAdapter {
+
+	private var dictionary: [String: Any]
+
+	init(
+		shortVersion: String?,
+		version: String?,
+		bundle: String?
+		) {
+		dictionary = [:]
+		dictionary["CFBundleShortVersionString"] = shortVersion
+		dictionary[kCFBundleVersionKey as String] = version
+		dictionary[kCFBundleIdentifierKey as String] = bundle
+	}
+
+	func object(forInfoDictionaryKey key: String) -> Any? {
+		dictionary[key]
+	}
+}
